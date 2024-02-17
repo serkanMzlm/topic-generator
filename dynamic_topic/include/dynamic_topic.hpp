97f8b5ae87806ce4adb34c87366173b975b67d60
serkan <serkanmazlum306@gmail.com>
2024-02-17 17:32:53 +0300

    New models have been added

--- dynamic_topic/include/dynamic_topic.hpp
+++ dynamic_topic/include/dynamic_topic.hpp
@@ -2,28 +2,39 @@
 #define __DYNAMIC_TOPIC__
 
 #include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/imu.hpp"
+#include "sensor_msgs/msg/range.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 #include "std_msgs/msg/float32.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
-#include "geometry_msgs/msg/twist.hpp"
 
 #define P2F(X) (1/X)
 
+using imuMsg = sensor_msgs::msg::Imu; 
+using rangeMsg = sensor_msgs::msg::Range; 
+using twistMsg = geometry_msgs::msg::Twist; 
 using float32Msg = std_msgs::msg::Float32; 
 using laser_scanMsg = sensor_msgs::msg::LaserScan; 
-using twistMsg = geometry_msgs::msg::Twist; 
 
 typedef struct{
+        rclcpp::Publisher<imuMsg>::SharedPtr imu;
+        rclcpp::Publisher<rangeMsg>::SharedPtr range;
         rclcpp::Publisher<twistMsg>::SharedPtr twist;
     
 }Pub_t;
 
 typedef struct{
+        rclcpp::Subscription<imuMsg>::SharedPtr imu;
+        rclcpp::Subscription<rangeMsg>::SharedPtr range;
+        rclcpp::Subscription<twistMsg>::SharedPtr twist;
         rclcpp::Subscription<float32Msg>::SharedPtr float32;
         rclcpp::Subscription<laser_scanMsg>::SharedPtr laser_scan;
     
 }Sub_t;
 
 typedef struct{
+        rclcpp::TimerBase::SharedPtr imu;
+        rclcpp::TimerBase::SharedPtr range;
         rclcpp::TimerBase::SharedPtr twist;
         
 }Time_t;
@@ -38,8 +49,13 @@ public:
     DynamicTopic();
     ~DynamicTopic();
 
+    void pubImuCallback();
+    void pubRangeCallback();
     void pubTwistCallback();
 
+    void subImuCallback(const imuMsg);
+    void subRangeCallback(const rangeMsg);
+    void subTwistCallback(const twistMsg);
     void subFloat32Callback(const float32Msg);
     void subLaserScanCallback(const laser_scanMsg);
 
--- dynamic_topic/src/dynamic_topic.cpp
+++ dynamic_topic/src/dynamic_topic.cpp
@@ -3,13 +3,25 @@
 using namespace std::placeholders;
 
 DynamicTopic::DynamicTopic(): Node("dynamic_topic_node"){
+    pub.imu = this->create_publisher<imuMsg>("/out/imu", 10);
+    pub.range = this->create_publisher<rangeMsg>("/out/range", 10);
     pub.twist = this->create_publisher<twistMsg>("/out/twist", 10);
 
+    sub.imu = this->create_subscription<imuMsg>("/in/imu", 10,
+                std::bind(&DynamicTopic::subImuCallback,this,_1)); 
+    sub.range = this->create_subscription<rangeMsg>("/in/range", 10,
+                std::bind(&DynamicTopic::subRangeCallback,this,_1)); 
+    sub.twist = this->create_subscription<twistMsg>("/in/twist", 10,
+                std::bind(&DynamicTopic::subTwistCallback,this,_1)); 
     sub.float32 = this->create_subscription<float32Msg>("/in/float32", 10,
                 std::bind(&DynamicTopic::subFloat32Callback,this,_1)); 
     sub.laser_scan = this->create_subscription<laser_scanMsg>("/in/lidar", 10,
                 std::bind(&DynamicTopic::subLaserScanCallback,this,_1)); 
 
+    timer.imu = this->create_wall_timer(std::chrono::milliseconds(P2F(100)),
+                        std::bind(&DynamicTopic::pubImuCallback, this));
+    timer.range = this->create_wall_timer(std::chrono::milliseconds(P2F(25)),
+                        std::bind(&DynamicTopic::pubRangeCallback, this));
     timer.twist = this->create_wall_timer(std::chrono::milliseconds(P2F(5)),
                         std::bind(&DynamicTopic::pubTwistCallback, this));
 
@@ -20,11 +32,28 @@ DynamicTopic::~DynamicTopic(){
 
 }
 
+void DynamicTopic::pubImuCallback(){
+    imuMsg msg;
+    pub.imu->publish(msg);
+}
+void DynamicTopic::pubRangeCallback(){
+    rangeMsg msg;
+    pub.range->publish(msg);
+}
 void DynamicTopic::pubTwistCallback(){
     twistMsg msg;
     pub.twist->publish(msg);
 }
 
+void DynamicTopic::subImuCallback(const imuMsg msg){
+    (void)msg;
+}
+void DynamicTopic::subRangeCallback(const rangeMsg msg){
+    (void)msg;
+}
+void DynamicTopic::subTwistCallback(const twistMsg msg){
+    (void)msg;
+}
 void DynamicTopic::subFloat32Callback(const float32Msg msg){
     (void)msg;
 }
